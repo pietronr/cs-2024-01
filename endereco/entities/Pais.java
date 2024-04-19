3555d9135e295f4f5e1257c165036730cae0b7fb
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2024-04-19 15:35:04 -0300

    Classe Pais implementada

---
+++ endereco/entities/Pais.java
@@ -0,0 +1,16 @@
+package endereco.entities;
+
+public class Pais {
+    // Atributo público para armazenar o nome do país
+    public String Nome;
+
+    // Método getter para obter o nome do país
+    public String getNome() {
+        return Nome;
+    }
+
+    // Método setter para definir o nome do país
+    public void setNome(String nome) {
+        this.Nome = nome;
+    }
+}
