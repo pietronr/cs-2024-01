bda7fdaf8f72d8acdffa2a14f51b7524de5c86d0
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2024-04-26 13:31:58 -0300

    Testes de login

---
+++ atividade08/tests/LoginTests.java
@@ -0,0 +1,42 @@
+package atividade08.tests;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import atividade08.classes.Login;
+import atividade08.exceptions.LoginInvalidoException;
+
+public class LoginTests {
+
+    @Test
+    public void testeLoginCorreto() {
+        Login login = new Login("usuario", "senha");
+        try {
+            boolean loginSucesso = login.fazerLogin("usuario", "senha");
+            assertTrue(loginSucesso);
+        } catch (LoginInvalidoException e) {
+            fail("Erro inesperado ao fazer login: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testeUsuarioIncorreto() {
+        Login login = new Login("usuario", "senha");
+        assertThrows(LoginInvalidoException.class, () -> login.fazerLogin("usuario_errado", "senha"));
+    }
+
+    @Test
+    public void testeSenhaIncorreta() {
+        Login login = new Login("usuario", "senha");
+        assertThrows(LoginInvalidoException.class, () -> login.fazerLogin("usuario", "senha_errada"));
+    }
+
+    @Test
+    public void testeUsuarioSenhaIncorretos() throws LoginInvalidoException {
+        Login login = new Login("usuario", "senha");
+        assertThrows(LoginInvalidoException.class, () -> login.fazerLogin("usuario_errado", "senha_errada"));
+    }
+}
